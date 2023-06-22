5a7d023280af122c9166536bd586e278fa6628ab
babulalt <babulal.tamang@berrybytes.com>
2023-06-22 08:22:29 +0545

    Javascript basic

---
+++ comparsion/comparasion.js
@@ -0,0 +1,8 @@
+console.log( 2 > 1 );  // true (correct)
+console.log( 2 == 1 ); // false (wrong)
+console.log( 2 != 1 ); // true (correct)
+
+console.log( 'Z' > 'A' ); // true
+console.log( 'Glow' > 'Glee' ); // true
+console.log( 'Bee' > 'Be' ); // true
+
---
+++ loop/foreach.js
@@ -0,0 +1,10 @@
+let sum = 0;
+const numbers = [65, 44, 12, 4];
+numbers.forEach(myFunction);
+
+function myFunction(item) {
+  sum += item;
+  console.log(item)
+}
+
+console.log(sum)
