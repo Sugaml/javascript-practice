998dff3af49a9f438c0eb4db0a51e1ddf3d83833
babulalt <babulal.tamang@berrybytes.com>
2023-06-26 08:29:40 +0545

    Switch case practice

---
+++ control/switch/switch.js
@@ -0,0 +1,15 @@
+let a=3+5
+
+switch (a){
+    case 3:
+        console.log("Small");
+        break;
+    case 8:
+        console.log("Exactly match");
+        break;
+    case 10:
+        console.log("Large");
+        break;
+    default:
+        console.log("No matching case.It is default case")
+}
---
+++ control/switch/without_break.js
@@ -0,0 +1,12 @@
+let a=3+5
+
+switch (a){
+    case 3:
+        console.log("Small");
+    case 8:
+        console.log("Exactly match");
+    case 10:
+        console.log("Large");
+    default:
+        console.log("No matching case.It is default case")
+}
